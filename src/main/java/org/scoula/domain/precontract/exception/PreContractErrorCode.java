dd3449757bf04ef96edfd714114d461c912c0270
LeeEunmi <eunmilee9109.1@gmail.com>
2025-07-24 13:38:27 +0900

    🐛 fix: 오타 수정

--- src/main/java/org/scoula/domain/precontract/exception/PreContractErrorCode.java
+++ src/main/java/org/scoula/domain/precontract/exception/PreContractErrorCode.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum PreContractErrorCode implements IErrorCode {
       TENANT_RESPONSE(
-              "TENANT_4001", HttpStatus.BAD_REQUEST, "DB에서 올바른 값을 가지오지 못해 response 값을 만들지 못했습니다."),
+              "TENANT_4001", HttpStatus.BAD_REQUEST, "DB에서 올바른 값을 가져오지 못해 response 값을 만들지 못했습니다."),
       ENUM_VALUE_OF("TENANT_4002", HttpStatus.BAD_REQUEST, "올바른 이넘 타입이 아닙니다. 다시 확인해주세요");
 
       private final String code;
