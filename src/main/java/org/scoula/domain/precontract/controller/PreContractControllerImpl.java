e5bc5e2aea6b92f4c84a3cc36af85a4993789b4c
minnieming <157681548+minnieming@users.noreply.github.com>
2025-07-25 13:26:53 +0900

    ✨ feat: 임차인 _ 계약서 사전 정보 저장 api (#13)
    * init
    
    * ✨ feat: [데이터베이스] DTO _ request / response
    
    * ✨ feat: [데이터베이스] VO
    
    * ✨ feat: [데이터베이스] mapper
    
    * ✨ feat: [데이터베이스] xml
    
    * ✨ feat: [데이터베이스] DatabaseConfig
    
    * ✨ feat: [서비스] DTO : VO -> DTO 추가
    
    * ✨ feat: [서비스] VO : DTO -> VO 추가
    
    * ✨ feat: [서비스] PreContractService / PreContractServiceImpl 생성
    
    * ✨ feat: [서비스] PreContractErrorCode 생성
    
    * ✨ feat: [서비스] RootConfig에 Service 등록
    
    * ✨ feat: [컨트롤러] PreContractController / PreContractControllerImpl 생성
    
    * ✨ feat: [컨트롤러] ServletConfig에 PreContractController 추가
    
    * ✨ feat: [서비스] Constants에 핸드폰 번호 상수 추가
    
    * 🌱 chore: [gitignore] .idea 추가
    
    * ♻ refactor: [mapper/xml] DTO, VO 클래스 파일명 변경
    
    * 🐛 fix: PreContractController 코드 포맷팅 수정
    
    * 🐛 fix: PreContractControllerImpl 코드 포맷팅 수정
    
    * 🐛 fix: auto increment id -> swagger에서 안보이게 하기
    
    * 🐛 fix: saveTenantInfo(service)에 트랜잭션 추가
    
    * 🐛 fix: enum -> valueof 예외처리 추가
    
    * 🐛 fix: 오타 수정
    
    * 🐛 fix: 메서드 파라미터값 수정
    
    * 🐛 fix: 오타 수정
    
    * 🐛 fix: 소문자 -> 대문자 수정
    
    * 🐛 fix: 오타 수정
    
    * 🐛 fix: 코드 포맷팅 에러 해결

---
+++ src/main/java/org/scoula/domain/precontract/controller/PreContractControllerImpl.java
@@ -0,0 +1,29 @@
+package org.scoula.domain.precontract.controller;
+
+import org.scoula.domain.precontract.dto.TenantPreContractDTO;
+import org.scoula.domain.precontract.service.PreContractService;
+import org.scoula.global.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/precon") // /api/도메인명
+@Log4j2
+public class PreContractControllerImpl implements PreContractController {
+
+      private final PreContractService service;
+
+      @Override
+      @PostMapping
+      public ResponseEntity<ApiResponse<TenantPreContractDTO>> saveTenantInfo(
+              @RequestBody TenantPreContractDTO tenantPreContractDTO) {
+          return ResponseEntity.ok(ApiResponse.success(service.saveTenantInfo(tenantPreContractDTO)));
+      }
+}
---
+++ src/main/java/org/scoula/domain/precontract/vo/TenantWolseInfoVO.java
@@ -0,0 +1,30 @@
+package org.scoula.domain.precontract.vo;
+
+import org.scoula.domain.precontract.dto.TenantPreContractDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TenantWolseInfoVO {
+
+      private Long tenantWolseRentId; // PK
+      private Long tenantPrecheckId; // FK
+
+      private Boolean wolseLoanPlan;
+      private Boolean wolseInsurancePlan;
+
+      // DTO -> VO
+      public static TenantWolseInfoVO toVO(TenantPreContractDTO dto, Long tenantPrecheckId) {
+          return TenantWolseInfoVO.builder()
+                  .tenantPrecheckId(tenantPrecheckId)
+                  .wolseLoanPlan(dto.getLoanPlan())
+                  .wolseInsurancePlan(dto.getInsurancePlan())
+                  .build();
+      }
+}
