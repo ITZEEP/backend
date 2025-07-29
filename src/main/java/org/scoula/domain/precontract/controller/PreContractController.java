a83fcd554a79dee24e51b9f3af1979b6bbaa7d5c
LeeEunmi <eunmilee9109.1@gmail.com>
2025-07-30 02:55:32 +0900

    ✨ feat: [임차인] 계약 전 controller/controllerImpl

--- src/main/java/org/scoula/domain/precontract/controller/PreContractController.java
+++ src/main/java/org/scoula/domain/precontract/controller/PreContractController.java
@@ -1,16 +1,76 @@
 package org.scoula.domain.precontract.controller;
 
 import org.scoula.domain.precontract.dto.TenantPreContractDTO;
+import org.scoula.domain.precontract.dto.TenantStep1DTO;
+import org.scoula.domain.precontract.dto.TenantStep2DTO;
+import org.scoula.domain.precontract.dto.TenantStep3DTO;
+import org.scoula.global.auth.dto.CustomUserDetails;
 import org.scoula.global.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 
 @Api(tags = "계약전 사전조사 API", description = "임대인/임차인 사전조사")
 public interface PreContractController {
 
-      @ApiOperation(value = "임차인 : 계약전 정보 저장", notes = "임차인의 계약전 사전조사 정보를 저장합니다.")
-      ResponseEntity<ApiResponse<TenantPreContractDTO>> saveTenantInfo(
-              TenantPreContractDTO tenantPreContractDTO);
+      // =============== 사기 위험도 확인 & 기본 세팅 ==================
+
+      @ApiModelProperty(value = "임차인 : step0 사기 위험도 조사 확인", notes = "임차인 사전조사 전 사기 위험도 조사 여부를 확인합니다")
+      ResponseEntity<ApiResponse<Boolean>> getCheckRisk(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiModelProperty(value = "임차인 : step0 데이터베이스 기본 세팅", notes = "임차인 사전조사 전 데이터베이스 행에 기본값을 넣어두기")
+      ResponseEntity<ApiResponse<String>> saveTenantInfo(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // =============== step 1 ==================
+
+      @ApiOperation(value = "임차인 : step1 페이지 저장", notes = "임차인의 계약전 조사 step1을 데이터베이스에 저장합니다.")
+      ResponseEntity<ApiResponse<Boolean>> updateTenantStep1(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody TenantStep1DTO step1DTO);
+
+      @ApiOperation(value = "임차인 : step1 페이지 조회", notes = "임차인의 계약전 조사 step1을 조회합니다.")
+      ResponseEntity<ApiResponse<TenantStep1DTO>> selectTenantStep1(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // =============== step 2 ==================
+
+      @ApiOperation(value = "임차인 : step2 페이지 저장", notes = "임차인의 계약전 조사 step2을 데이터베이스에 저장합니다.")
+      ResponseEntity<ApiResponse<Void>> updateTenantStep2(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody TenantStep2DTO step2DTO);
+
+      @ApiOperation(value = "임차인 : step2 페이지 조회", notes = "임차인의 계약전 조사 step2을 조회합니다.")
+      ResponseEntity<ApiResponse<TenantStep2DTO>> selectTenantStep2(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // =============== step 3 ==================
+      @ApiOperation(value = "임차인 : step3 페이지 저장", notes = "임차인의 계약전 조사 step3을 데이터베이스에 저장합니다.")
+      ResponseEntity<ApiResponse<Void>> updateTenantStep3(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody TenantStep3DTO step3DTO);
+
+      @ApiOperation(value = "임차인 : step3 페이지 조회", notes = "임차인의 계약전 조사 step3을 조회합니다.")
+      ResponseEntity<ApiResponse<TenantStep3DTO>> selectTenantStep3(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // =============== 최종 ==================
+      @ApiOperation(value = "임차인 : 최종 페이지 조회", notes = "임차인의 계약전 조사 최종 페이지를 조회합니다.")
+      ResponseEntity<ApiResponse<TenantPreContractDTO>> selectTenantPreCon(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
 }
--- src/main/java/org/scoula/domain/precontract/controller/PreContractControllerImpl.java
+++ src/main/java/org/scoula/domain/precontract/controller/PreContractControllerImpl.java
@@ -1,29 +1,127 @@
 package org.scoula.domain.precontract.controller;
 
 import org.scoula.domain.precontract.dto.TenantPreContractDTO;
+import org.scoula.domain.precontract.dto.TenantStep1DTO;
+import org.scoula.domain.precontract.dto.TenantStep2DTO;
+import org.scoula.domain.precontract.dto.TenantStep3DTO;
 import org.scoula.domain.precontract.service.PreContractService;
+import org.scoula.global.auth.dto.CustomUserDetails;
 import org.scoula.global.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/precon") // /api/도메인명
+@RequestMapping("/api/pre-contract/{contractChatId}/buyer") // /api/도메인명
 @Log4j2
 public class PreContractControllerImpl implements PreContractController {
 
       private final PreContractService service;
 
+      // =============== 사기 위험도 확인 & 기본 세팅 ==================
+
       @Override
-      @PostMapping
-      public ResponseEntity<ApiResponse<TenantPreContractDTO>> saveTenantInfo(
-              @RequestBody TenantPreContractDTO tenantPreContractDTO) {
-          return ResponseEntity.ok(ApiResponse.success(service.saveTenantInfo(tenantPreContractDTO)));
+      @GetMapping("/check-risk")
+      public ResponseEntity<ApiResponse<Boolean>> getCheckRisk(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(service.getCheckRisk(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PostMapping("/init-con")
+      public ResponseEntity<ApiResponse<String>> saveTenantInfo(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.saveTenantInfo(contractChatId, userDetails.getUserId())));
+          //            return
+          // ResponseEntity.ok(ApiResponse.success(service.saveTenantInfo(contractChatId,
+          // userDetails.getUserId())))
+      }
+
+      // =============== step 1 ==================
+
+      @Override
+      @PatchMapping("/step1")
+      public ResponseEntity<ApiResponse<Boolean>> updateTenantStep1(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody TenantStep1DTO step1DTO) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.updateTenantStep1(
+                                  contractChatId, userDetails.getUserId(), step1DTO)));
+      }
+
+      @Override
+      @GetMapping("/step1")
+      public ResponseEntity<ApiResponse<TenantStep1DTO>> selectTenantStep1(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.selectTenantStep1(contractChatId, userDetails.getUserId())));
+      }
+
+      // =============== step 2 ==================
+      @Override
+      @PatchMapping("/step2")
+      public ResponseEntity<ApiResponse<Void>> updateTenantStep2(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody TenantStep2DTO step2DTO) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.updateTenantStep2(
+                                  contractChatId, userDetails.getUserId(), step2DTO)));
+      }
+
+      @Override
+      @GetMapping("/step2")
+      public ResponseEntity<ApiResponse<TenantStep2DTO>> selectTenantStep2(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.selectTenantStep2(contractChatId, userDetails.getUserId())));
+      }
+
+      // =============== step 3 ==================
+
+      @Override
+      @PatchMapping("/step3")
+      public ResponseEntity<ApiResponse<Void>> updateTenantStep3(
+              Long contractChatId, CustomUserDetails userDetails, TenantStep3DTO step3DTO) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.updateTenantStep3(
+                                  contractChatId, userDetails.getUserId(), step3DTO)));
+      }
+
+      @Override
+      @GetMapping("/step3")
+      public ResponseEntity<ApiResponse<TenantStep3DTO>> selectTenantStep3(
+              Long contractChatId, CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.selectTenantStep3(contractChatId, userDetails.getUserId())));
+      }
+
+      // =============== 최종 ==================
+
+      @Override
+      @GetMapping("/pre-con")
+      public ResponseEntity<ApiResponse<TenantPreContractDTO>> selectTenantPreCon(
+              Long contractChatId, CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.selectTenantPreCon(contractChatId, userDetails.getUserId())));
       }
 }
