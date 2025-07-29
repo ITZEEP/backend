1e86cb6376aad3081c1790f3e5fde3bfbdc2c340
LeeEunmi <eunmilee9109.1@gmail.com>
2025-07-30 02:54:22 +0900

    ✨ feat: [임차인] 계약 전 exception

--- src/main/java/org/scoula/domain/precontract/exception/PreContractErrorCode.java
+++ src/main/java/org/scoula/domain/precontract/exception/PreContractErrorCode.java
@@ -11,7 +11,11 @@ import lombok.RequiredArgsConstructor;
 public enum PreContractErrorCode implements IErrorCode {
       TENANT_RESPONSE(
               "TENANT_4001", HttpStatus.BAD_REQUEST, "DB에서 올바른 값을 가져오지 못해 response 값을 만들지 못했습니다."),
-      ENUM_VALUE_OF("TENANT_4002", HttpStatus.BAD_REQUEST, "올바른 이넘 타입이 아닙니다. 다시 확인해주세요");
+      ENUM_VALUE_OF("TENANT_4002", HttpStatus.BAD_REQUEST, "올바른 이넘 타입이 아닙니다. 다시 확인해주세요"),
+      TENANT_UPDATE("TENANT_4003", HttpStatus.BAD_REQUEST, "값이 저장되지 않았습니다. (update fail)"),
+      TENANT_RISK("TENANT_4004", HttpStatus.BAD_REQUEST, "사기위험도 검사 결과가 없습니다"),
+      TENANT_INSERT("TENANT_4005", HttpStatus.BAD_REQUEST, "데이터베이스 저장 실패 (insert fail)"),
+      TENANT_USER("TENANT_4006", HttpStatus.BAD_REQUEST, "데이터베이스 유저와 현재 유저가 다릅니다");
 
       private final String code;
       private final HttpStatus httpStatus;
