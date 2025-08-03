05cce13dd6ba81c8e7613bfb3bf6902b6061cdb9
Whatdoyumin <lym4963@naver.com>
2025-08-03 23:10:13 +0900

    ♻ refactor: interface에 있는 get, post, patch, delete 매핑 제거

--- src/main/java/org/scoula/domain/precontract/controller/OwnerPreContractController.java
+++ src/main/java/org/scoula/domain/precontract/controller/OwnerPreContractController.java
@@ -9,10 +9,7 @@ import org.scoula.domain.precontract.dto.owner.OwnerPreContractDTO;
 import org.scoula.global.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.annotations.Api;
@@ -22,7 +19,6 @@ import io.swagger.annotations.ApiOperation;
 public interface OwnerPreContractController {
       // =============== 데이터베이스 세팅 ==================
       @ApiOperation(value = "임대인 : step0 데이터베이스 기본 세팅", notes = "임대인 사전조사 전 데이터베이스 행에 기본값을 넣어두기")
-      @PostMapping
       ResponseEntity<ApiResponse<String>> saveOwnerInfo(
               @PathVariable Long contractChatId, Authentication authentication);
 
@@ -30,7 +26,6 @@ public interface OwnerPreContractController {
       @ApiOperation(
               value = "임대인 : 계약 정보 설정 step 1 페이지 저장",
               notes = "임대인 사전조사 계약 정보 설정 step1을 데이터베이스에 저장합니다.")
-      @PatchMapping
       ResponseEntity<ApiResponse<Boolean>> updateOwnerContractStep1(
               @PathVariable Long contractChatId,
               Authentication authentication,
@@ -39,7 +34,6 @@ public interface OwnerPreContractController {
       @ApiOperation(
               value = "임대인 : 계약 정보 설정 step 1 페이지 조회",
               notes = "임대인 사전조사 계약 정보 설정 step 1을 조회합니다.")
-      @GetMapping
       ResponseEntity<ApiResponse<OwnerContractStep1DTO>> selectOwnerContractStep1(
               @PathVariable Long contractChatId, Authentication authentication);
 
@@ -47,7 +41,6 @@ public interface OwnerPreContractController {
       @ApiOperation(
               value = "임대인 : 계약 정보 설정 step 2 페이지 저장",
               notes = "임대인 사전조사 계약 정보 설정 step2을 데이터베이스에 저장합니다.")
-      @PostMapping
       ResponseEntity<ApiResponse<Boolean>> updateOwnerContractStep2(
               @PathVariable Long contractChatId,
               Authentication authentication,
@@ -56,7 +49,6 @@ public interface OwnerPreContractController {
       @ApiOperation(
               value = "임대인 : 계약 정보 설정 step 2 페이지 조회",
               notes = "임대인 사전조사 계약 정보 설정 step 2을 조회합니다.")
-      @GetMapping
       ResponseEntity<ApiResponse<OwnerContractStep2DTO>> selectOwnerContractStep2(
               @PathVariable Long contractChatId, Authentication authentication);
 
@@ -64,7 +56,6 @@ public interface OwnerPreContractController {
       @ApiOperation(
               value = "임대인 : 거주 정보 설정 step 1 페이지 저장",
               notes = "임대인 사전조사 거주 정보 설정 step 1을 데이터베이스에 저장합니다.")
-      @PatchMapping
       ResponseEntity<ApiResponse<Boolean>> updateOwnerLivingStep1(
               @PathVariable Long contractChatId,
               Authentication authentication,
@@ -73,18 +64,15 @@ public interface OwnerPreContractController {
       @ApiOperation(
               value = "임대인 : 거주 정보 설정 step 1 페이지 조회",
               notes = "임대인 사전조사 거주 정보 설정 step 1을 조회합니다.")
-      @GetMapping
       ResponseEntity<ApiResponse<OwnerLivingStep1DTO>> selectOwnerLivingStep1(
               @PathVariable Long contractChatId, Authentication authentication);
 
       // =============== 계약서 특약 내용 ==================
       @ApiOperation(value = "임대인 : 계약서 특약 내용 몽고DB 조회", notes = "OCR로 추출된 특약 내용을 조회합니다.")
-      @GetMapping
       ResponseEntity<ApiResponse<ContractDocumentMongoDocument>> getContractDocument(
               @PathVariable Long contractChatId, Authentication authentication);
 
       @ApiOperation(value = "임대인 : 계약서 특약 내용 몽고DB에 저장", notes = "OCR로 추출된 특약 내용을 저장합니다.")
-      @PostMapping
       ResponseEntity<ApiResponse<Boolean>> saveContractDocument(
               @PathVariable Long contractChatId,
               Authentication authentication,
@@ -92,13 +80,11 @@ public interface OwnerPreContractController {
 
       // =============== 최종 정보 확인 ==================
       @ApiOperation(value = "임대인 : 최종 페이지 조회", notes = "임대인의 계약전 조사 최종 페이지를 조회합니다.")
-      @GetMapping
       ResponseEntity<ApiResponse<OwnerPreContractDTO>> selectOwnerContract(
               @PathVariable Long contractChatId, Authentication authentication);
 
       // =============== 최종 정보 MongoDB로 저장 ==================
       @ApiOperation(value = "임대인 : 최종 데이터 몽고DB에 저장", notes = "임대인의 계약전 작성 사항을 AI로 보내기 위해 몽고DB에 저장한다.")
-      @PostMapping
       ResponseEntity<ApiResponse<Void>> saveMongoDB(
               @PathVariable Long contractChatId, Authentication authentication);
 }
