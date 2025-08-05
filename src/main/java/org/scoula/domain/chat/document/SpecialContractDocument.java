3f3b34198d643fac4a7d821dc31a3c0218ce7409
MeongW <easymiracle@skuniv.ac.kr>
2025-08-05 15:02:55 +0900

    ♻️ refactor: SpecialContractDocument 중복 코드 제거 및 구조 개선

--- src/main/java/org/scoula/domain/chat/document/SpecialContractDocument.java
+++ src/main/java/org/scoula/domain/chat/document/SpecialContractDocument.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -16,13 +20,9 @@ import lombok.*;
 public class SpecialContractDocument {
 
       @Id private String id;
-
       private Long contractChatId;
-
       private Long round;
-
       private Integer totalClauses;
-
       private List<Clause> clauses;
 
       @Getter
@@ -31,7 +31,7 @@ public class SpecialContractDocument {
       @AllArgsConstructor
       @Builder
       public static class Clause {
-          private Integer id;
+          private Integer order;
           private String title;
           private String content;
           private Assessment assessment;
