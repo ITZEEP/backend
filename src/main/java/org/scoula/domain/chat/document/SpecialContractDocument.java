370f331c76ae2ec6b779c093ea827cc60bee7ca3
leeedongjaee <harold306122@gmail.com>
2025-08-05 10:19:53 +0900

    ✨ feat: 추출된 특약 저장 도큐먼트

---
+++ src/main/java/org/scoula/domain/chat/document/SpecialContractDocument.java
@@ -0,0 +1,59 @@
+package org.scoula.domain.chat.document;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "SPECIAL_CONTRACT")
+public class SpecialContractDocument {
+
+      @Id private String id;
+
+      private Long contractChatId;
+
+      private Long round;
+
+      private Integer totalClauses;
+
+      private List<Clause> clauses;
+
+      @Getter
+      @Setter
+      @NoArgsConstructor
+      @AllArgsConstructor
+      @Builder
+      public static class Clause {
+          private Integer id;
+          private String title;
+          private String content;
+          private Assessment assessment;
+      }
+
+      @Getter
+      @Setter
+      @NoArgsConstructor
+      @AllArgsConstructor
+      @Builder
+      public static class Assessment {
+          private Evaluation owner;
+          private Evaluation tenant;
+      }
+
+      @Getter
+      @Setter
+      @NoArgsConstructor
+      @AllArgsConstructor
+      @Builder
+      public static class Evaluation {
+          private String level;
+          private String reason;
+      }
+}
