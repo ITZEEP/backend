3f64973310c6eea2a42b8fad2b4c57dc540912c6
leeedongjaee <harold306122@gmail.com>
2025-08-06 18:59:47 +0900

    ✨ feat: 계약 사전 조사 URL 메시지 추가

--- src/main/java/org/scoula/domain/chat/service/ChatServiceImpl.java
+++ src/main/java/org/scoula/domain/chat/service/ChatServiceImpl.java
@@ -813,7 +813,7 @@ public class ChatServiceImpl implements ChatServiceInterface {
                           .build();
 
           handleChatMessage(acceptMessage);
-          String contractChatUrl = "/contract-chat/" + contractChatRoomId.toString();
+          String contractChatUrl = "http://localhost:5173/pre-contract/" + contractChatRoomId.toString()+"/owner?step=1"+"/n"+"http://localhost:5173/pre-contract/" + contractChatRoomId.toString()+"/buyer?step=1";
 
           ChatMessageRequestDto linkMessage =
                   ChatMessageRequestDto.builder()
