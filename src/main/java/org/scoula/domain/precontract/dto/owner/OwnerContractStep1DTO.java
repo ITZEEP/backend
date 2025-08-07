84b0ffedb71256c2277dbc5d2803520d293acb4d
Whatdoyumin <lym4963@naver.com>
2025-08-08 01:39:54 +0900

    ✨ feat: 미납 국세 지방세 여부 확인, 선순위 확정일자 여부 추가

--- src/main/java/org/scoula/domain/precontract/dto/owner/OwnerContractStep1DTO.java
+++ src/main/java/org/scoula/domain/precontract/dto/owner/OwnerContractStep1DTO.java
@@ -39,4 +39,16 @@ public class OwnerContractStep1DTO {
       @ApiModelProperty(value = "비품 수리 책임 주체", required = true, example = "OWNER")
       @NotNull
       private ResponsibilityParty responseRepairingFixtures;
+
+      @ApiModelProperty(value = "국세/지방세 미납 여부", required = true)
+      @NotNull
+      private Boolean hasTaxArrears;
+
+      @ApiModelProperty(value = "국세/지방세 미납 금액", required = true, example = "0")
+      @NotNull
+      private int taxArrearsAmount;
+
+      @ApiModelProperty(value = "선순위 확정 일자 현황 여부", required = true)
+      @NotNull
+      private Boolean hasPriorFixedDate;
 }
