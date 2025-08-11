38955a58d7cda651983b1e88d4cba1ffdedec8cf
LeeEunmi <eunmilee9109.1@gmail.com>
2025-08-11 15:30:56 +0900

    ✨ feat: 계약서 mapper / xml

---
+++ src/main/java/org/scoula/domain/contract/mapper/ContractMapper.java
@@ -0,0 +1,30 @@
+package org.scoula.domain.contract.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.scoula.domain.contract.dto.ContractDTO;
+
+@Mapper
+public interface ContractMapper {
+
+      ContractDTO getContract(@Param("contractChatId") Long contractChatId);
+
+      String getDuration(@Param("contractChatId") Long contractChatId);
+
+      int updateStatus(@Param("contractChatId") Long contractChatId, @Param("step") int step);
+
+      Long selectFinalContractId(@Param("contractChatId") Long contractChatId);
+
+      int insertSignatureInit(@Param("contractChatId") Long contractChatId);
+
+      int updateTaxSignature(
+              @Param("finalContractId") Long finalContractId,
+              @Param("url") String url,
+              @Param("hashKey") String hashKey);
+
+      int insertFinalContract(@Param("contractChatId") Long contractChatId);
+
+      String selectOwnerTaxSignatureUrl(@Param("finalContractId") Long finalContractId);
+
+      boolean getDepositAdjustment(@Param("contractChatId") Long contractChatId);
+}
