99e051a702923c613c4f143bb7eeef031646aca2
LeeEunmi <eunmilee9109.1@gmail.com>
2025-08-11 15:26:36 +0900

    ✨ feat: 계약서 0,1,2단계 controller / controllerImpl

---
+++ src/main/java/org/scoula/domain/contract/controller/ContractController.java
@@ -0,0 +1,94 @@
+package org.scoula.domain.contract.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.scoula.domain.contract.dto.*;
+import org.scoula.global.auth.dto.CustomUserDetails;
+import org.scoula.global.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "계약서 API", description = "계약서 : 정보확인 / 금액 조율 / 적법성 확인")
+public interface ContractController {
+
+      // step 0
+      @ApiOperation(value = "임차인 대기 메세지", notes = "step0의 AI 메세지")
+      ResponseEntity<ApiResponse<Void>> standByContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // step 1 (init)
+      @ApiOperation(value = "계약서 몽고DB에 저장", notes = "계약서에 필요한 항목들을 가져와서 몽고 DB에 계약서 만들기")
+      ResponseEntity<ApiResponse<Void>> saveContractMongo(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // step 1 : start
+      @ApiOperation(value = "계약서 전체 조회", notes = "계약서 가져오기")
+      ResponseEntity<ApiResponse<ContractDTO>> getContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiOperation(value = "정보조회 다음 단계로 넘어가기 Message", notes = "정보조회 마지막 단계에서 다음 단계로 넘어가기 Message")
+      ResponseEntity<ApiResponse<Void>> getContractNext(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      // step 1 : finish
+      @ApiOperation(value = "다음 단계로 넘어가기", notes = "다음 단계 여부(true/false)를 받아서 다음 단계로 넘어가기")
+      ResponseEntity<ApiResponse<Boolean>> nextStep(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody NextStepDTO dto);
+
+      @ApiOperation(value = "금액 조회", notes = "금액을 조율하기 위해 금액을 조회")
+      ResponseEntity<ApiResponse<PaymentDTO>> getDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiOperation(value = "금액 요청", notes = "임대인이 금액을 요청")
+      ResponseEntity<ApiResponse<Void>> saveDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody PaymentDTO dto);
+
+      @ApiOperation(value = "금액 거절 ", notes = "임차인이 금액을 거절")
+      ResponseEntity<ApiResponse<Void>> deleteDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiOperation(value = "금액 수락", notes = "임대인과 임차인 모두 동의")
+      ResponseEntity<ApiResponse<Void>> updateDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiOperation(value = "AI 적법성 확인 from 몽고DB", notes = "몽고DB에 있는 계약서를 AI로 보내고, 적법성 받기")
+      ResponseEntity<ApiResponse<LegalityDTO>> getLegality(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiOperation(value = "특약을 계약서 DB에 저장 FROM 몽고DB", notes = "특약 테이블에 있는걸 계약서로 가져오기")
+      ResponseEntity<ApiResponse<Void>> saveSpecialContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+      @ApiOperation(value = "바뀐 특약 수정 from 몽고DB", notes = "적법성 검사 후 수정된 특약으로 변경")
+      ResponseEntity<ApiResponse<Void>> updateSpecialContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody SpecialContractDTO dto);
+
+      @ApiOperation(value = "적법성 검사 후 다음단계로 넘어가기", notes = "적법성 검사 후 AI 메세지를 보낸다")
+      ResponseEntity<ApiResponse<Void>> sendStep4(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails);
+
+
+}
---
+++ src/main/java/org/scoula/domain/contract/controller/ContractControllerImpl.java
@@ -0,0 +1,156 @@
+package org.scoula.domain.contract.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.scoula.domain.contract.dto.*;
+import org.scoula.domain.contract.service.ContractService;
+import org.scoula.global.auth.dto.CustomUserDetails;
+import org.scoula.global.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/api/contract/{contractChatId}")
+public class ContractControllerImpl implements ContractController {
+
+      private final ContractService service;
+
+      @Override
+      @GetMapping("/standBy")
+      public ResponseEntity<ApiResponse<Void>> standByContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.standByContract(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PostMapping("")
+      public ResponseEntity<ApiResponse<Void>> saveContractMongo(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.saveContractMongo(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @GetMapping("")
+      public ResponseEntity<ApiResponse<ContractDTO>> getContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(service.getContract(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @GetMapping("/step1")
+      public ResponseEntity<ApiResponse<Void>> getContractNext(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.getContractNext(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PostMapping("/nextStep")
+      public ResponseEntity<ApiResponse<Boolean>> nextStep(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody NextStepDTO dto) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.nextStep(contractChatId, userDetails.getUserId(), dto)));
+      }
+
+      @Override
+      @GetMapping("/price")
+      public ResponseEntity<ApiResponse<PaymentDTO>> getDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.getDepositPrice(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PostMapping("/price")
+      public ResponseEntity<ApiResponse<Void>> saveDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody PaymentDTO dto) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.saveDepositPrice(contractChatId, userDetails.getUserId(), dto)));
+      }
+
+      @Override
+      @DeleteMapping("/price")
+      public ResponseEntity<ApiResponse<Void>> deleteDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.deleteDepositPrice(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PatchMapping("/price")
+      public ResponseEntity<ApiResponse<Void>> updateDepositPrice(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.updateDepositPrice(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PostMapping("/legality")
+      public ResponseEntity<ApiResponse<LegalityDTO>> getLegality(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(service.getLegality(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PostMapping("/specialContract")
+      public ResponseEntity<ApiResponse<Void>> saveSpecialContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.saveSpecialContract(contractChatId, userDetails.getUserId())));
+      }
+
+      @Override
+      @PatchMapping("/specialContract")
+      public ResponseEntity<ApiResponse<Void>> updateSpecialContract(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails,
+              @RequestBody SpecialContractDTO dto) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(
+                          service.updateSpecialContract(
+                                  contractChatId, userDetails.getUserId(), dto)));
+      }
+
+      @Override
+      @GetMapping("/specialContract")
+      public ResponseEntity<ApiResponse<Void>> sendStep4(
+              @PathVariable Long contractChatId,
+              @AuthenticationPrincipal CustomUserDetails userDetails) {
+          return ResponseEntity.ok(
+                  ApiResponse.success(service.sendStep4(contractChatId, userDetails.getUserId())));
+      }
+
+}
