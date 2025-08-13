d02cfc4ed55be6e28b53c2e77ed6b565f640e546
leeedongjaee <harold306122@gmail.com>
2025-08-13 15:11:09 +0900

    ✨ feat: ROUND4 추가

--- src/main/java/org/scoula/domain/chat/vo/ContractChat.java
+++ src/main/java/org/scoula/domain/chat/vo/ContractChat.java
@@ -46,6 +46,8 @@ public class ContractChat {
                   return 3L;
               case ROUND3:
                   return 4L;
+              case ROUND4:
+                  return 5L;
               default:
                   return 1L;
           }
@@ -57,6 +59,7 @@ public class ContractChat {
                   && (status == ContractStatus.ROUND0
                           || status == ContractStatus.ROUND1
                           || status == ContractStatus.ROUND2
-                          || status == ContractStatus.ROUND3);
+                          || status == ContractStatus.ROUND3
+                          || status == ContractStatus.ROUND4);
       }
 }
