d3226a3dd752af2241382a1b19124b2256e04719
LeeEunmi <eunmilee9109.1@gmail.com>
2025-08-18 01:03:44 +0900

    ✨ feat: 내보내기 exception

--- src/main/java/org/scoula/domain/contract/exception/ContractException.java
+++ src/main/java/org/scoula/domain/contract/exception/ContractException.java
@@ -14,7 +14,12 @@ public enum ContractException implements IErrorCode {
       CONTRACT_AI_SERVER_ERROR(
               "CONTRACT_4003", HttpStatus.SERVICE_UNAVAILABLE, "AI 서버 통신 중 오류가 발생했습니다."),
       CONTRACT_UPDATE("CONTRACT_4004", HttpStatus.BAD_REQUEST, "MongoDB에 수정이 되지 않았습니다"),
-      CONTRACT_REDIS("CONTRACT_4005", HttpStatus.BAD_REQUEST, "REDIS에 해당 정보가 없습니다.");
+      CONTRACT_REDIS("CONTRACT_4005", HttpStatus.BAD_REQUEST, "REDIS에 해당 정보가 없습니다."),
+      CONTRACT_DB_INSERT("CONTRACT_4006", HttpStatus.BAD_REQUEST, "DB에 저장되지 않았습니다."),
+      CONTRACT_DB_UPDATE("CONTRACT_4007", HttpStatus.BAD_REQUEST, "DB에 수정되지 않았습니다."),
+      CONTRACT_AGREEMENT(
+              "CONTRACT_4008", HttpStatus.BAD_REQUEST, "최종 계약서에 동의가 되지 않아 계약서를 완료할 수 없습니다.");
+
       private final String code;
       private final HttpStatus httpStatus;
       private final String message;
