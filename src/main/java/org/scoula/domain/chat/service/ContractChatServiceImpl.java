7040a7f47feac05f5c602e39553f7abe9dc07e15
leeedongjaee <harold306122@gmail.com>
2025-08-18 11:11:42 +0900

    ✨ feat: 최종 계약 수락 및 거절 로직 수정

--- src/main/java/org/scoula/domain/chat/service/ContractChatServiceImpl.java
+++ src/main/java/org/scoula/domain/chat/service/ContractChatServiceImpl.java
@@ -2860,7 +2860,7 @@ public class ContractChatServiceImpl implements ContractChatServiceInterface {
               throw new IllegalArgumentException("최종 특약서가 생성되지 않았습니다.");
           }
 
-          AiMessageBtn(contractChatId, "임대인이 최종 특약 확정을 요청하였습니다");
+          AiMessageBtn(contractChatId, "임대인이 최종 계약서 확인을 요청하였습니다");
 
           String key = "final-contract:request:" + contractChatId;
           String existingValue = stringRedisTemplate.opsForValue().get(key);
